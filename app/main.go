ee554d408c72fccb433837f550927cd2d2f3d856
tung <tung@duy-tung.dev>
2025-04-10 03:56:12 +0000

    remove redundant comment

--- app/main.go
+++ app/main.go
@@ -13,8 +13,6 @@ var _ = os.Exit
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	Uncomment this block to pass the first stage
 	
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
